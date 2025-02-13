f16246073fc3c9846440aa69f2afb6b13bdf88b9
justcode <147803636+imjustcodingg@users.noreply.github.com>
2025-02-13 20:56:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const cursor = document.querySelector(".cursor");
+
+// FUNCTION TO MOVE THE CURSOR
+function moveCursor(x, y) {
+  cursor.style.left = x - 100 + "px";
+  cursor.style.top = y - 100 + "px";
+}
+
+// MOUSE SUPPORT
 window.addEventListener("mousemove", function (e) {
-  cursor.style.left = e.x - 100 + "px";
-  cursor.style.top = e.y - 100 + "px";
+  moveCursor(e.clientX, e.clientY);
+});
+
+// TOUCH SUPPORT
+window.addEventListener("touchmove", function (e) {
+  if (e.touches.length > 0) {
+    const touch = e.touches[0]; // GET FIRST TOUCH POINT
+    moveCursor(touch.clientX, touch.clientY);
+  }
 });
